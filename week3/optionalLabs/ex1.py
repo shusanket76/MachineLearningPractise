f1e63c4891702da60c18dcf07b62dd923fd58190
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2023-08-14 16:21:12 -0400

    softmaxLogitsLinear

--- week3/optionalLabs/ex1.py
+++ week3/optionalLabs/ex1.py
@@ -1,6 +1,9 @@
 from sklearn.datasets import make_blobs
 from multiclasslab import *
 import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+import tensorflow as tf
 
 classes=4
 m=100
@@ -9,4 +12,30 @@ std = 1.0
 xtrain,ytrain = make_blobs(n_samples=m, centers=centers)
 # print(xtrain)
 # plt_mc(xtrain,ytrain, classes, centers, std=1.0)
-print(np.unique(ytrain))
+print(ytrain[:10])
+print(xtrain.shape)
+print(ytrain.shape)
+
+model = Sequential(
+    [
+        Dense(2, activation="relu", name="layer1"),
+        Dense(4, activation="linear", name="linearlayer2")
+    ]
+)
+
+model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              optimizer=tf.keras.optimizers.legacy.Adam(0.01))
+
+model.fit(xtrain,ytrain,epochs=200)
+w1,b1= model.get_layer("layer1").get_weights()
+print(w1)
+print("====================")
+print(b1)
+
+
+plt_cat_mc(xtrain,ytrain, model, classes)
+plt_layer_relu(xtrain,ytrain,w1,b1,classes)
+
+l2 = model.get_layer("linearlayer2")
+w2,b2 = l2.get_weights()
+x12 = np.maximum(0,np.dot(xtrain,w1)+b1)
